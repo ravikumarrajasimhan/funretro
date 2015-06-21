ddaed6f2fb4c3e9798d27070d5340e78363005ae
Glauber Ramos <glauberamos@gmail.com>
2015-06-20 20:32:42 -0400

    adding voting and delete features

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ app.controller("SampleCtrl", function($scope, $firebaseArray) {
   var ref = new Firebase("https://blinding-torch-6662.firebaseio.com/messages");
 
   $scope.messages = $firebaseArray(ref);
+  $scope.messageTypes = [1, 2, 3];
 
   $scope.addMessage = function() {
     $scope.messages.$add({
-      text: $scope.newMessageText
+      text: $scope.newMessageText,
+      type: $scope.newMessageType,
+      votes: 0
     });
   };
+
+  $scope.addVote = function(key, votes) {
+  	if(!localStorage.getItem(key)) {
+  		ref.child(key).update({ votes: votes + 1});
+  		localStorage.setItem(key, 1);
+  	}
+  }
 });
