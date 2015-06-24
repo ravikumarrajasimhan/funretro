1c5bec69b5e6c1f5dce7b6976ac1e4c8c261f6a3
Glauber Ramos <glauberamos@gmail.com>
2015-06-23 23:48:04 -0400

    improving order and animation when voting/deleting/adding, improve imports js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-var app = angular.module("sampleApp", ["firebase"]);
+var app = angular.module("sampleApp", ['firebase']);
 
-app.controller("MainCtrl", function($scope, $firebaseArray) {
+app.controller("MainCtrl", ["$firebaseArray", '$scope', '$filter', function($firebaseArray, $scope, $filter) {
   var ref = new Firebase("https://blinding-torch-6662.firebaseio.com/messages");
 
   $scope.boardId = window.location.hash.substring(1);
-
   $scope.messages = $firebaseArray(ref);
+
+  function calculateAllHeights(messages) {
+    var orderedArray = $filter('orderBy')(messages, ['-votes', 'date']);
+     orderedArray.forEach(function(message) {
+      var filtered = orderedArray.filter(function(item) {
+        return item.type.id === message.type.id;
+      });
+
+      message.currentHeight = filtered.indexOf(message) * 150 + 150 + 'px';
+      $scope.messages.$save(message);
+    });
+  }
+
+  $scope.messages.$loaded().then(function(messages) {
+    calculateAllHeights(messages);
+  });
+
   $scope.messageTypes = [{
   	id: 1,
   	value: "Went well"
@@ -19,15 +35,27 @@ app.controller("MainCtrl", function($scope, $firebaseArray) {
 
   $scope.addVote = function(key, votes) {
   	if(!localStorage.getItem(key)) {
-  		ref.child(key).update({ votes: votes + 1});
+  		ref.child(key).update({ votes: votes + 1, date: Firebase.ServerValue.TIMESTAMP });
   		localStorage.setItem(key, 1);
   	}
+
+    calculateAllHeights($scope.messages);
   }
 
   $scope.deleteMessage = function(message) {
-  	if(confirm('Are you sure you want to delete?')) {
-  		$scope.messages.$remove(message);
+  	if(confirm('Are you sure you want to delete this note?')) {
+  		$scope.messages.$remove(message).then(function() {
+        calculateAllHeights($scope.messages);    
+      });
   	}
+  }
+
+  $scope.getHeight = function(message, index) {
+    if(!message.currentHeight) {
+      return index * 150 + 150 + 'px';
+    } else {
+      return message.currentHeight;
+    }
   }
 
   $scope.alreadyVoted = function(key) {
@@ -38,7 +66,10 @@ app.controller("MainCtrl", function($scope, $firebaseArray) {
   	$scope.messages.$add({
       text: '',
       type: type,
+      date: Firebase.ServerValue.TIMESTAMP,
       votes: 0
+    }).then(function() {
+      calculateAllHeights($scope.messages);  
     });
   }
-});
+}]);
