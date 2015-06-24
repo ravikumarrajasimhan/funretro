3784fe0cc313c44616fbd38695509839e90e61d5
Glauber Ramos <glauberamos@gmail.com>
2015-06-24 00:51:37 -0400

    adding shift + enter logic on edition

--- script.js
+++ script.js
@@ -77,3 +77,17 @@ app.controller("MainCtrl", ["$firebaseArray", '$scope', '$filter', function($fir
     });
   }
 }]);
+
+app.directive('enterClick', function () {
+  return {
+    restrict: 'A',
+    link: function (scope, elem, attrs) {
+      elem.bind('keydown', function(event) {
+        if (event.keyCode === 13 && event.shiftKey) {
+          event.preventDefault();
+          $(elem[0]).find('button').click();
+        }
+      });
+    }
+  }
+});
