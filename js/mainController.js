d20d53d5b247cab62d3effee260338022b0832ba
Glauber Ramos <glauberamos@gmail.com>
2015-09-02 14:47:56 -0300

    Adding utils file

--- js/mainController.js
+++ js/mainController.js
@@ -1,227 +1,179 @@
-angular.module("fireideaz", ['firebase', 'ngDialog'])
+angular
+  .module('fireideaz')
+  .controller('MainCtrl', ['$firebaseArray', '$scope', '$filter', '$window', 'ngDialog', 'Utils',
+    function($firebaseArray, $scope, $filter, $window, ngDialog, utils) {
+      var mainRef = new Firebase("https://firedeaztest.firebaseio.com");
+      var messagesRef = new Firebase("https://firedeaztest.firebaseio.com/messages");
+      var boardRef = new Firebase("https://firedeaztest.firebaseio.com/boards");
+      
+      $scope.userId = $window.location.hash.substring(1) || '';
+      $scope.messageTypes = utils.messageTypes;
+      $scope.utils = utils;
 
-.controller("MainCtrl", ["$firebaseArray", '$scope', '$filter', '$window', 'ngDialog',
-  function($firebaseArray, $scope, $filter, $window, ngDialog) {
-    var mainRef = new Firebase("https://firedeaztest.firebaseio.com");
-    var messagesRef = new Firebase("https://firedeaztest.firebaseio.com/messages");
-    var boardRef = new Firebase("https://firedeaztest.firebaseio.com/boards");
-    
-    $scope.userId = $window.location.hash.substring(1) || '';
+      mainRef.authWithPassword({
+        email    : $scope.userId + '@fireideaz.com',
+        password : $scope.userId
+      }, function(error, authData) {
+        if (!error) {
+          $scope.boards = $firebaseArray(boardRef);
+          $scope.messages = $firebaseArray(messagesRef.orderByChild("user_id").equalTo($scope.userId));
+          $scope.board = $firebaseArray(boardRef.orderByChild("user_id").equalTo($scope.userId));
 
-    mainRef.authWithPassword({
-      email    : $scope.userId + '@fireideaz.com',
-      password : $scope.userId
-    }, function(error, authData) {
-      if (!error) {
-        $scope.boards = $firebaseArray(boardRef);
+          $scope.newBoard = {
+            name: ''
+          };  
+
+          $scope.board.$loaded().then(function() {
+            $scope.boardId = $scope.board[0].boardId;
+          });
+
+          $scope.messages.$loaded().then(function(messages) {
+            calculateAllHeights(messages);
+          });
+        }
+      });
+
+      $scope.boardNameChanged = function() {
+        $scope.newBoard.name = $scope.newBoard.name.replace(/\s+/g,'');
+      }
+
+      $scope.createNewBoard = function() {
+        var newUser = utils.createUserId();
+        $scope.newBoard.name;
+
+        mainRef.createUser({
+          email    : newUser + '@fireideaz.com',
+          password : newUser
+        }, function(error, userData) {
+          if (!error) {
+            mainRef.authWithPassword({
+              email    : $scope.userId + '@fireideaz.com',
+              password : $scope.userId
+            }, function(error, authData) {
+              if (!error) {
+                $scope.userId = newUser;
+
+                $scope.boards.$add({
+                  boardId: $scope.newBoard.name,
+                  columns: $scope.messageTypes,
+                  user_id: $scope.userId
+                });
+
+                window.location.href = window.location.origin + window.location.pathname + "#" + newUser;
+                ngDialog.closeAll();
+
+                $scope.newBoard.name = '';
+              }
+            });
+          }
+        });
+      };
+
+      $scope.addNewColumn = function(name) {
+        var board = $scope.boards.$getRecord($scope.board[0].$id);
+        board.columns.push({
+          value: name,
+          id: $scope.getNextId()
+        });
+
+        $scope.boards.$save(board).then(function() {
+          ngDialog.closeAll();
+        });
+      };
+
+      function calculateAllHeights(messages) {
+        var orderedArray = $filter('orderBy')(messages, ['-votes', 'date']);
+         orderedArray.forEach(function(message) {
+          var filtered = orderedArray.filter(function(item) {
+            return item.type.id === message.type.id;
+          });
+
+          message.currentHeight = filtered.indexOf(message) * 125 + 120 + 'px';
+          $scope.messages.$save(message);
+        });
+      }
+
+      $scope.addVote = function(key, votes) {
+      	if(!localStorage.getItem(key)) {
+      		messagesRef.child(key).update({ votes: votes + 1, date: Firebase.ServerValue.TIMESTAMP });
+      		localStorage.setItem(key, 1);
+      	}
+
+        calculateAllHeights($scope.messages);
+      }
+
+      $scope.openDialogColumn = function() {
+        ngDialog.open({
+          template: 'addNewColumn',
+          className: 'ngdialog-theme-plain',
+          scope: $scope
+        });
+      }
+
+      $scope.openDialogBoard = function() {
+        ngDialog.open({
+          template: 'addNewBoard',
+          className: 'ngdialog-theme-plain',
+          scope: $scope
+        });
+      }
+
+      $scope.getNextId = function() {
+        return $scope.board[0].columns[$scope.board[0].columns.length -1].id + 1;
+      }
+
+      $scope.changeColumnName = function(id, newName) {
+        var board = $scope.boards.$getRecord($scope.board[0].$id);
+        board.columns[id - 1].value = newName;
+
+        $scope.boards.$save(board).then(function() {
+          ngDialog.closeAll();
+        });
+      };
+
+      $scope.deleteLastColumn = function() {
+        if(confirm('Are you sure you want to delete this column?')) {
+          var board = $scope.boards.$getRecord($scope.board[0].$id);
+          board.columns.pop();
+          $scope.boards.$save(board);
+        }
+      };
+
+      $scope.deleteMessage = function(message) {
+      	if(confirm('Are you sure you want to delete this note?')) {
+      		$scope.messages.$remove(message).then(function() {
+            calculateAllHeights($scope.messages);    
+          });
+      	}
+      }
+
+      $scope.addNew = function(type) {
+      	$scope.messages.$add({
+          text: '',
+          board: $scope.boardId,
+          user_id: $scope.userId,
+          type: {
+            id: type.id
+          },
+          date: Firebase.ServerValue.TIMESTAMP,
+          votes: 0
+        }).then(function(message) {
+          var id = message.key();
+          angular.element($('#' + id)).scope().isEditing = true;
+          $('#' + id).find('textarea').focus();
+
+          calculateAllHeights($scope.messages);  
+        });
+      };
+
+      $($window).bind('hashchange', function () {
+        $scope.userId = $window.location.hash.substring(1);
         $scope.messages = $firebaseArray(messagesRef.orderByChild("user_id").equalTo($scope.userId));
         $scope.board = $firebaseArray(boardRef.orderByChild("user_id").equalTo($scope.userId));
-
-        $scope.newBoard = {
-          name: ''
-        };  
 
         $scope.board.$loaded().then(function() {
           $scope.boardId = $scope.board[0].boardId;
         });
-
-        $scope.messages.$loaded().then(function(messages) {
-          calculateAllHeights(messages);
-        });
-      }
-    });
-
-    $scope.boardNameChanged = function() {
-      $scope.newBoard.name = $scope.newBoard.name.replace(/\s+/g,'');
-    }
-
-    function createUserId() {
-      var text = "";
-      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-      for( var i=0; i < 5; i++ ) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-      }
-
-      return text;
-    };
-
-    $scope.createNewBoard = function() {
-      var newUser = createUserId();
-      $scope.newBoard.name;
-
-      mainRef.createUser({
-        email    : newUser + '@fireideaz.com',
-        password : newUser
-      }, function(error, userData) {
-        if (!error) {
-          mainRef.authWithPassword({
-            email    : $scope.userId + '@fireideaz.com',
-            password : $scope.userId
-          }, function(error, authData) {
-            if (!error) {
-              $scope.userId = newUser;
-
-              $scope.boards.$add({
-                boardId: $scope.newBoard.name,
-                columns: $scope.messageTypes,
-                user_id: $scope.userId
-              });
-
-              window.location.href = window.location.origin + window.location.pathname + "#" + newUser;
-              ngDialog.closeAll();
-
-              $scope.newBoard.name = '';
-            }
-          });
-        }
       });
-    };
-
-    $scope.addNewColumn = function(name) {
-      var board = $scope.boards.$getRecord($scope.board[0].$id);
-      board.columns.push({
-        value: name,
-        id: $scope.getNextId()
-      });
-
-      $scope.boards.$save(board).then(function() {
-        ngDialog.closeAll();
-      });
-    };
-
-    function calculateAllHeights(messages) {
-      var orderedArray = $filter('orderBy')(messages, ['-votes', 'date']);
-       orderedArray.forEach(function(message) {
-        var filtered = orderedArray.filter(function(item) {
-          return item.type.id === message.type.id;
-        });
-
-        message.currentHeight = filtered.indexOf(message) * 125 + 120 + 'px';
-        $scope.messages.$save(message);
-      });
-    }
-
-    $scope.messageTypes = [{
-    	id: 1,
-    	value: "Went well"
-    }, { 
-    	id: 2,
-    	value: "To improve"
-    }, { 
-    	id: 3,
-    	value: "Action Items"
-    }];
-
-    $scope.addVote = function(key, votes) {
-    	if(!localStorage.getItem(key)) {
-    		messagesRef.child(key).update({ votes: votes + 1, date: Firebase.ServerValue.TIMESTAMP });
-    		localStorage.setItem(key, 1);
-    	}
-
-      calculateAllHeights($scope.messages);
-    }
-
-    $scope.openDialogColumn = function() {
-      ngDialog.open({
-        template: 'addNewColumn',
-        className: 'ngdialog-theme-plain',
-        scope: $scope
-      });
-    }
-
-    $scope.openDialogBoard = function() {
-      ngDialog.open({
-        template: 'addNewBoard',
-        className: 'ngdialog-theme-plain',
-        scope: $scope
-      });
-    }
-
-    $scope.getNextId = function() {
-      return $scope.board[0].columns[$scope.board[0].columns.length -1].id + 1;
-    }
-
-    $scope.changeColumnName = function(id, newName) {
-      var board = $scope.boards.$getRecord($scope.board[0].$id);
-      board.columns[id - 1].value = newName;
-
-      $scope.boards.$save(board).then(function() {
-        ngDialog.closeAll();
-      });
-    };
-
-    $scope.deleteLastColumn = function() {
-      if(confirm('Are you sure you want to delete this column?')) {
-        var board = $scope.boards.$getRecord($scope.board[0].$id);
-        board.columns.pop();
-        $scope.boards.$save(board);
-      }
-    };
-
-    $scope.showRemoveColumn = function(id) {
-      var board = $scope.boards.$getRecord($scope.board[0].$id);
-
-      if(board.columns.length === id) {
-        if(board.columns.length > 3) {
-          return true;
-        }
-      }
-
-      return false;
-    };
-
-    $scope.deleteMessage = function(message) {
-    	if(confirm('Are you sure you want to delete this note?')) {
-    		$scope.messages.$remove(message).then(function() {
-          calculateAllHeights($scope.messages);    
-        });
-    	}
-    }
-
-    $scope.getHeight = function(message, index) {
-      if(!message.currentHeight) {
-        return index * 125 + 120 + 'px';
-      } else {
-        return message.currentHeight;
-      }
-    }
-
-    $scope.alreadyVoted = function(key) {
-    	return localStorage.getItem(key);
-    }
-
-    $scope.focusElement = function(id) {
-      $('#' + id).find('textarea').focus();
-    };
-
-    $scope.addNew = function(type) {
-    	$scope.messages.$add({
-        text: '',
-        board: $scope.boardId,
-        user_id: $scope.userId,
-        type: {
-          id: type.id
-        },
-        date: Firebase.ServerValue.TIMESTAMP,
-        votes: 0
-      }).then(function(message) {
-        var id = message.key();
-        angular.element($('#' + id)).scope().isEditing = true;
-        $('#' + id).find('textarea').focus();
-
-        calculateAllHeights($scope.messages);  
-      });
-    };
-
-    $($window).bind('hashchange', function () {
-      $scope.userId = $window.location.hash.substring(1);
-      $scope.messages = $firebaseArray(messagesRef.orderByChild("user_id").equalTo($scope.userId));
-      $scope.board = $firebaseArray(boardRef.orderByChild("user_id").equalTo($scope.userId));
-
-      $scope.board.$loaded().then(function() {
-        $scope.boardId = $scope.board[0].boardId;
-      });
-    });
-  }]
-);
+    }]
+  );
---
+++ js/utils.js
@@ -0,0 +1,60 @@
+angular
+  .module('fireideaz')
+  .service('Utils', function () {
+    function createUserId() {
+      var text = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      for( var i=0; i < 5; i++ ) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+
+      return text;
+    };
+
+    function getHeight(message, index) {
+      if(!message.currentHeight) {
+        return index * 125 + 120 + 'px';
+      } else {
+        return message.currentHeight;
+      }
+    }
+
+    function alreadyVoted(key) {
+      return localStorage.getItem(key);
+    }
+
+    function focusElement(id) {
+      $('#' + id).find('textarea').focus();
+    };
+
+    var messageTypes = [{
+      id: 1,
+      value: "Went well"
+    }, { 
+      id: 2,
+      value: "To improve"
+    }, { 
+      id: 3,
+      value: "Action Items"
+    }];
+
+    function showRemoveColumn(id, columns) {
+      if(columns.length === id) {
+        if(columns.length > 3) {
+          return true;
+        }
+      }
+
+      return false;
+    };
+
+    return {
+      createUserId: createUserId,
+      getHeight: getHeight,
+      alreadyVoted: alreadyVoted,
+      focusElement: focusElement,
+      messageTypes: messageTypes,
+      showRemoveColumn: showRemoveColumn
+    };
+  });
