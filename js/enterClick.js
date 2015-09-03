15ceab95b03e9f369e07f0788a290e4661864628
Glauber Ramos <glauberamos@gmail.com>
2015-09-03 15:36:51 -0300

    Fixing bug with continuing editing a note

--- js/enterClick.js
+++ js/enterClick.js
@@ -7,6 +7,7 @@ angular
       elem.bind('keydown', function(event) {
         if (event.keyCode === 13 && event.shiftKey) {
           event.preventDefault();
+          $(elem[0]).find('button').focus();
           $(elem[0]).find('button').click();
         }
       });
