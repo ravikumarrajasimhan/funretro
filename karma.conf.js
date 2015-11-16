6e811f336cb28d8ecbd5f205802799a2b4f8fcbf
Glauber Ramos <glauberamos@gmail.com>
2015-11-15 22:20:02 -0200

    Adding coveralls #44

--- karma.conf.js
+++ karma.conf.js
@@ -3,10 +3,10 @@ module.exports = function(config) {
 
     config.set({
         frameworks: ['jasmine'],
-        reporters: ['spec', 'coverage'],
+        reporters: ['spec', 'coverage', 'coveralls'],
         browsers: ['PhantomJS'],
         coverageReporter: {
-          //repoToken: 'khhm0L9sekEGN2r12dWspLyYT7D7cK2KA',
+          repoToken: 'QVdqIxSZvbUFLmSiYZ3uINtguZxhuBgy7',
           type: 'lcov',
           dir: 'coverage/'
         },
