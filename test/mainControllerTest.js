be95f378f5544e3bdf8f38f47008a30063ef2aa2
Bruna <ppbruna2@gmail.com>
2016-02-15 14:58:41 -0200

    adds some tests to mainController

--- test/mainControllerTest.js
+++ test/mainControllerTest.js
@@ -1,15 +1,41 @@
-describe('Main controller', function() {
-    beforeEach(angular.mock.module('fireideaz'));
+describe('MainCtrl: ', function() {
 
-    beforeEach(angular.mock.inject(function($rootScope, $controller){
-        scope = $rootScope.$new();
-        $controller('MainCtrl', {$scope: scope});
-    }));
+  var $rootScope,
+      $scope,
+      $controller,
+      $firebaseArray,
+      board;
 
-  it('change new board name to remove spaces', function() {
-      scope.newBoard.name = 'new name';
-      expect(scope.newBoard.name).toBe('new name');
-      scope.boardNameChanged();
-      expect(scope.newBoard.name).toBe('newname');
+  beforeEach(angular.mock.module('fireideaz'));
+
+  beforeEach(inject(function($injector){
+    $rootScope = $injector.get('$rootScope');
+    $scope = $rootScope.$new();
+    $controller = $injector.get('$controller');
+
+    $controller('MainCtrl', {
+      '$scope': $scope,
+    });
+  }));
+
+  it('should strip spaces from board names', function() {
+    $scope.newBoard.name = 'new name';
+
+    $scope.boardNameChanged();
+
+    expect($scope.newBoard.name).to.equal('newname');
+  });
+
+
+  it('should return true when sort order by votes', function() {
+    $scope.sortField = 'votes';
+
+    expect($scope.getSortOrder()).to.be.true;
+  });
+
+  it('should return false when sort order is not by votes', function() {
+    $scope.sortField = 'something else';
+
+    expect($scope.getSortOrder()).to.be.false;
   });
 });
