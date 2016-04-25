46c32fc4fe1505d84e95a81e933579254303d7e8
Glauber Ramos <glauberamos@gmail.com>
2016-04-25 02:23:30 -0300

    change karma log back to info

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function(config) {
     colors: true,
 
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['PhantomJS'],
     singleRun: false,
