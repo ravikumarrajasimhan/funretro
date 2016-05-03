4d818d0f5cf3f591e8eb60e7abbfa0914065772c
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-05-03 16:57:08 -0700

    Separate webpack config for dist and dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var directories = {
-  app: path.resolve(__dirname, 'app')
+  app: path.resolve(__dirname, 'app'),
+  dist: path.resolve(__dirname, 'dist')
 };
 
 module.exports = {
+  directories: directories,
   devtool: 'sourcemap',
   context: directories.app,
   entry: './index.js',
-  output: {
-    path: directories.app,
-    filename: 'bundle.js'
-  },
   module: {
     loaders: [
-      { test: /.js$/, exclude: [/node_modules/], loader: 'ng-annotate!babel' }
+      { test: /.js$/, exclude: [/node_modules/], loader: 'ng-annotate!babel' },
+      { test: /.html$/, loader: 'raw' }
     ]
   },
   devServer: {
-    contentBase: directories.app,
-  }
+    contentBase: directories.app
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: directories.app + '/index.html',
+      inject: 'body',
+      hash: true
+    })
+  ]
 };
+
