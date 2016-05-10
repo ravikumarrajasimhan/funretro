276b6dca7d034d81852f8a15efef09e4c4ce1bf6
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-05-10 15:14:39 -0300

    Adds modal templates

--- app/services/ModalService.js
+++ app/services/ModalService.js
@@ -2,50 +2,49 @@ const ModalService = (ngDialog) => {
   return {
     openAddNewColumn: (scope) => {
       ngDialog.open({
-        template: 'addNewColumn',
+        templateUrl: './views/modals/add-column.html',
         className: 'ngdialog-theme-plain',
         scope: scope
       });
     },
     openAddNewBoard: (scope) => {
       ngDialog.open({
-        template: 'addNewBoard',
+        templateUrl: './views/modals/create-board.html',
         className: 'ngdialog-theme-plain',
         scope: scope
       });
     },
     openDeleteCard: (scope) => {
       ngDialog.open({
-        template: 'deleteCard',
+        templateUrl: './views/modals/delete-card.html',
         className: 'ngdialog-theme-plain',
         scope: scope
       });
     },
     openDeleteColumn: (scope) => {
       ngDialog.open({
-        template: 'deleteColumn',
+        templateUrl: './views/modals/delete-column.html',
         className: 'ngdialog-theme-plain',
         scope: scope
       });
     },
-
     openMergeCards: (scope) => {
       ngDialog.open({
-        template: 'mergeCards',
-        className: 'ngdialog-theme-plain',
+        templateUrl: './views/modals/merge-cards.html',
+        plain: true,
         scope: scope
       });
     },
-    openCopyBoard: (scope) => {
+    openExportBoard: (scope) => {
       ngDialog.open({
-        template: 'copyBoard',
+        templateUrl: './views/modals/export-board.html',
         className: 'ngdialog-theme-plain bigDialog',
         scope: scope
       });
     },
     openDeleteCards: (scope) => {
       ngDialog.open({
-        template: 'deleteCards',
+        templateUrl: './views/modals/delete-cards.html',
         className: 'ngdialog-theme-plain danger',
         scope: scope
       });
