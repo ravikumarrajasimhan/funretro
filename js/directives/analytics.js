ab549338cecc90db54fd68bd6a87738aba602ee8
Glauber Ramos <glauberamos@gmail.com>
2016-05-12 00:03:16 -0300

    removing analytics directive, fixing header on top of loading and removing domain

--- js/directives/analytics.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('fireideaz').directive('analytics', [function() {
-    return {
-      restrict: 'E',
-      templateUrl : 'components/analytics.html'
-    };
-  }]
-);
