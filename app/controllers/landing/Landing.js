68044ba1e6770723dab0bac32b8c8b92457b43e7
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-05-13 12:14:05 -0300

    Adds board redirect after creation

--- app/controllers/landing/Landing.js
+++ app/controllers/landing/Landing.js
@@ -1,8 +1,12 @@
-const Landing = ($scope, modalService, boardService) => {
+const Landing = ($scope, modalService, boardService, $window) => {
 
   $scope.modalService = modalService;
   $scope.board = {
     name: ''
+  };
+
+  const openBoard = () => {
+    $window.location.href = `/#${$scope.board.id}`;
   };
 
   $scope.createNewBoard = () => {
@@ -11,11 +15,12 @@ const Landing = ($scope, modalService, boardService) => {
     boardService.connect();
     boardService.create($scope.board.name).then((boardID) => {
       $scope.board.id = boardID;
+      openBoard();
     });
   };
 
 };
 
-Landing.$inject = ['$scope', 'modalService', 'boardService'];
+Landing.$inject = ['$scope', 'modalService', 'boardService', '$window'];
 
 export default Landing;
