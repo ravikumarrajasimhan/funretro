9afca6f329aeb2513e7ad83885e60d7f97083463
Glauber Ramos <glauberamos@gmail.com>
2016-08-01 01:08:48 -0300

    change karma to log debug

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function(config) {
     colors: true,
 
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
     autoWatch: true,
     browsers: ['PhantomJS'],
     singleRun: false,
