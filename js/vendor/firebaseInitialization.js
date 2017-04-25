87480f616bd019dbf54743cda18fbab40b070fb5
Lane Sawyer <lanesawyer@gmail.com>
2017-04-25 13:13:11 -0600

    Fixing tests by moving Firebase initialization code

---
+++ js/vendor/firebaseInitialization.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyDKlqYdC-wUUOT7E7aRmRtEDtK6N8CAZ3U",
+  authDomain: "fun-retro-6216f.firebaseapp.com",
+  databaseURL: "https://fun-retro-6216f.firebaseio.com",
+  projectId: "fun-retro-6216f",
+  storageBucket: "fun-retro-6216f.appspot.com",
+  messagingSenderId: "741022796633"
+};
+
+firebase.initializeApp(config);
