c6f4af438a9b777f3e52c7e1ba2a29a29a83b54c
Glauber Ramos <glauberamos@gmail.com>
2017-05-01 23:56:44 -0300

    Removing mock of firebaseService and auth

--- test/mainControllerTest.js
+++ test/mainControllerTest.js
@@ -12,38 +12,6 @@ describe('MainCtrl: ', function() {
   beforeEach(function() {
     angular.mock.module('fireideaz');
 
-    angular.mock.module(function($provide) {
-      $provide.service('FirebaseService', function() {
-        this.newFirebaseArray = function(messagesRef) {
-          return [];
-        };
-        this.getServerTimestamp = function() {
-          return '00:00:00';
-        };
-        this.getMessagesRef = function(userId) {
-          return [];
-        };
-        this.getMessageRef = function(userId, messageId) {
-          return [];
-        };
-        this.getBoardRef = function(userId) {
-          return [];
-        };
-        this.getBoardColumns = function(userId) {
-          return [];
-        };
-      });
-
-      $provide.service('Auth', function() {
-        this.logUser = function(newUser, callback) {
-          return "mock";
-        };
-        this.createUserAndLog = function(newUser, callback) {
-          return "mock";
-        };
-      });
-    });
-
     inject(function($injector) {
       $rootScope = $injector.get('$rootScope');
       $scope = $rootScope.$new();
