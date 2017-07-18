76958e34aace8b1b779f8da1f25ce3c962fcb63f
Glauber dutra ramos <glauberamos@gmail.com>
2017-07-18 04:39:51 -0300

    Fix bug with opening multiple boards

--- js/mainController.js
+++ js/mainController.js
@@ -59,9 +59,6 @@ angular
           $scope.boardId = $rootScope.boardId = board.val().boardId;
           $scope.boardContext = $rootScope.boardContext = board.val().boardContext;
           $scope.loading = false;
-        }, function() {
-          window.location.hash = '';
-          location.reload();
         });
 
         $scope.boardRef = board;
