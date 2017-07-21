10fcc667c96293957179c29a502695991c144806
Glauber dutra ramos <glauberamos@gmail.com>
2017-07-20 21:21:13 -0300

    Remove vote function to voteService

--- js/vendor/firebaseInitialization.js
+++ js/vendor/firebaseInitialization.js
@@ -1,10 +1,10 @@
 var config = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGE_ID"
+   apiKey: "AIzaSyA-Z_L7624wJIuJszgn95VMOoxAcj-ywDw",
+   authDomain: "funretrotest.firebaseapp.com",
+   databaseURL: "https://funretrotest.firebaseio.com",
+   projectId: "funretrotest",
+   storageBucket: "funretrotest.appspot.com",
+   messagingSenderId: "444255922815"
  };
 
  firebase.initializeApp(config);
