80c7a93ee83fe894a15578491c33219725479973
Glauber dutra ramos <glauberamos@gmail.com>
2017-11-19 21:22:59 -0200

    Updates from ecdba2b Glauber dutra ramos Adding funretro.io

--- main.js
+++ main.js
@@ -454,6 +454,119 @@ angular
 
 'use strict';
 
+angular.module('fireideaz').directive('about', [function() {
+    return {
+      templateUrl : 'components/about.html'
+    };
+  }]
+);
+
+'use strict';
+
+angular.module('fireideaz').directive('focus', function($timeout) {
+    return function(scope, element) {
+       scope.$watch('editing',
+         function () {
+            $timeout(function() {
+                element[0].focus();
+            }, 0, false);
+         });
+      };
+});
+
+'use strict';
+
+angular.module('fireideaz').directive('boardContext', [function() {
+    return {
+      restrict: 'E',
+      templateUrl : 'components/boardContext.html'
+    };
+  }]
+);
+
+'use strict';
+
+angular.module('fireideaz').directive('dialogs', ['ImportExportService', function(importExportService) {
+    return {
+      restrict: 'E',
+      templateUrl : 'components/dialogs.html',
+      link: function($scope) {
+        $scope.importExportService = importExportService;
+      }
+    };
+  }]
+);
+
+'use strict';
+
+angular.module('fireideaz').directive('pageHeader', ['ModalService', function(modalService) {
+    return {
+      templateUrl : 'components/header.html',
+      link: function($scope) {
+        $scope.modalService = modalService;
+      }
+    };
+  }]
+);
+
+'use strict';
+
+angular.module('fireideaz').directive('mainContent', [function() {
+    return {
+      templateUrl : 'components/mainContent.html'
+    };
+  }]
+);
+
+'use strict';
+
+angular.module('fireideaz').directive('mainPage', ['ModalService', function(modalService) {
+    return {
+      restrict: 'E',
+      templateUrl : 'components/mainPage.html',
+      link: function($scope) {
+        $scope.modalService = modalService;
+      }
+    };
+  }]
+);
+
+'use strict';
+
+angular.module('fireideaz').directive('menu', ['VoteService', function(voteService) {
+    return {
+      templateUrl : 'components/menu.html',
+      link: function($scope) {
+        $scope.voteService = voteService;
+      }
+    };
+  }]
+);
+
+'use strict';
+
+angular.module('fireideaz').directive('sidebar', ['ModalService', function(modalService) {
+    return {
+      templateUrl : 'components/sidebar.html',
+      link: function($scope) {
+        $scope.modalService = modalService;
+      }
+    };
+  }]
+);
+
+'use strict';
+
+angular.module('fireideaz').directive('spinner', [function() {
+    return {
+      restrict: 'E',
+      templateUrl : 'components/spinner.html'
+    };
+  }]
+);
+
+'use strict';
+
 angular
   .module('fireideaz')
   .service('FirebaseService', ['firebase', '$firebaseArray', '$firebaseObject', function (firebase, $firebaseArray, $firebaseObject) {
@@ -885,116 +998,3 @@ angular
 
     return voteService;
   }]);
-
-'use strict';
-
-angular.module('fireideaz').directive('about', [function() {
-    return {
-      templateUrl : 'components/about.html'
-    };
-  }]
-);
-
-'use strict';
-
-angular.module('fireideaz').directive('focus', function($timeout) {
-    return function(scope, element) {
-       scope.$watch('editing',
-         function () {
-            $timeout(function() {
-                element[0].focus();
-            }, 0, false);
-         });
-      };
-});
-
-'use strict';
-
-angular.module('fireideaz').directive('boardContext', [function() {
-    return {
-      restrict: 'E',
-      templateUrl : 'components/boardContext.html'
-    };
-  }]
-);
-
-'use strict';
-
-angular.module('fireideaz').directive('dialogs', ['ImportExportService', function(importExportService) {
-    return {
-      restrict: 'E',
-      templateUrl : 'components/dialogs.html',
-      link: function($scope) {
-        $scope.importExportService = importExportService;
-      }
-    };
-  }]
-);
-
-'use strict';
-
-angular.module('fireideaz').directive('pageHeader', ['ModalService', function(modalService) {
-    return {
-      templateUrl : 'components/header.html',
-      link: function($scope) {
-        $scope.modalService = modalService;
-      }
-    };
-  }]
-);
-
-'use strict';
-
-angular.module('fireideaz').directive('mainContent', [function() {
-    return {
-      templateUrl : 'components/mainContent.html'
-    };
-  }]
-);
-
-'use strict';
-
-angular.module('fireideaz').directive('mainPage', ['ModalService', function(modalService) {
-    return {
-      restrict: 'E',
-      templateUrl : 'components/mainPage.html',
-      link: function($scope) {
-        $scope.modalService = modalService;
-      }
-    };
-  }]
-);
-
-'use strict';
-
-angular.module('fireideaz').directive('menu', ['VoteService', function(voteService) {
-    return {
-      templateUrl : 'components/menu.html',
-      link: function($scope) {
-        $scope.voteService = voteService;
-      }
-    };
-  }]
-);
-
-'use strict';
-
-angular.module('fireideaz').directive('sidebar', ['ModalService', function(modalService) {
-    return {
-      templateUrl : 'components/sidebar.html',
-      link: function($scope) {
-        $scope.modalService = modalService;
-      }
-    };
-  }]
-);
-
-'use strict';
-
-angular.module('fireideaz').directive('spinner', [function() {
-    return {
-      restrict: 'E',
-      templateUrl : 'components/spinner.html'
-    };
-  }]
-);
